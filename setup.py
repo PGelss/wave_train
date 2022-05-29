d6f52a1bfd8f46862a0a27fa8a00b5f8105e1471
Burkhard Schmidt <55391388+bsch63@users.noreply.github.com>
2022-05-29 13:19:34 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,8 @@
+# ED stands for ExciDyn (package for exciton dynamics)
+# To install a development version run: pip install -e .
+# in the root directory
+# Import command : import ED.foo
+from setuptools import setup
+
+if __name__ == "__main__":
+    setup()
