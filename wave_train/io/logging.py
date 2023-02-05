2baf74871c095704c8d40aeded98819e8c126bd9
Jerome Riedel <jerome@fhi-berlin.mpg.de>
2023-02-05 16:25:07 +0100

    Function and class docs for logging handlers

--- wave_train/io/logging.py
+++ wave_train/io/logging.py
@@ -2,23 +2,43 @@ import sys
 from typing import Callable
 
 def handle_logging(func: Callable):
+    """
+    Wrapper function that can be used to automatically log calls output to
+    stdout in a logfile specified by the argument log_file. Argument must
+    be explicitly provided. Logging type can be changed by providing the
+    to_console argument in kwargs of func.
+
+    func: Callable
+        function with kwargs optionality
+    
+
+    log_file: str
+        Name of the logfile  
+    to_console: boolean
+        If True, output will also be written to stdout, otherwise output
+        will only be written to logfile
+
+    Returns
+    =======
+        The result of func
+    """
     def logging(*args, **kwargs):
         log_file    = ""
-        teeing      = False
+        to_console  = False
 
         if "log_file" in kwargs:
             log_file = kwargs["log_file"]
 
-        if "split" in kwargs:
-            teeing = kwargs["split"]
+        if "to_console" in kwargs:
+            to_console = kwargs["to_console"]
         else:
-            teeing = True 
+            to_console = True 
 
-        if log_file and teeing:
+        if log_file and to_console:
             logger = TeeLogger(log_file)
             return func(*args, **kwargs)
 
-        elif log_file and not teeing:
+        elif log_file and not to_console:
             logger = Logger(log_file)
             return func(*args, **kwargs)
         else:
@@ -27,7 +47,15 @@ def handle_logging(func: Callable):
     return logging
 
 class TeeLogger:
-    def __init__(self, log_file) -> None:
+    """
+    Class for handling logging to a logfile while also writing to stdout.
+    Previous value of stdout will be restored once TeeLogger is
+    garbage collected.
+
+    log_file: str
+        Name of logfile
+    """
+    def __init__(self, log_file: str) -> None:
         self.log_file = log_file
         self.stdout  = sys.stdout
         sys.stdout   = self
@@ -46,6 +74,15 @@ class TeeLogger:
         self.file.flush()
 
 class Logger:
+    """
+    Class for handling logging to a logfile. All output to stdout
+    will be redirected to the logfile.
+    Previous value of stdout will be restored once Logger instance is
+    garbage collected.
+
+    log_file: str
+        Name of logfile
+    """
     def __init__(self, log_file) -> None:
         self.log_file = log_file 
         self.stdout  = sys.stdout
